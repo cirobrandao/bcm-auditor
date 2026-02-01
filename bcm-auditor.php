61addf4089612142cef2815256c37c17d724cc3c
Roboto <roboto@bcmnetwork.com.br>
2026-02-01 02:53:48 +0000

    Update plugin headers: Author z/ONE, Author URI dev.zone.net.br, Plugin URI GitHub

--- bcm-auditor.php
+++ bcm-auditor.php
@@ -6,7 +6,8 @@
  * Version:           0.1.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Author:            BCM Network
+ * Author:            z/ONE
+ * Author URI:        https://dev.zone.net.br/wordpress
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       bcm-auditor
